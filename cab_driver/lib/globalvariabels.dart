019e7972d6d601c7aea79e36bf53ff210751720f
robert1564 <52401139+robert1564@users.noreply.github.com>
2021-09-23 12:43:25 +0300

    Updates and Fixes

---
+++ cab_driver/lib/globalvariabels.dart
@@ -0,0 +1,32 @@
+
+import 'dart:async';
+
+import 'package:assets_audio_player/assets_audio_player.dart';
+import 'package:cab_driver/datamodels/driver.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+User currentFirebaseUser;
+
+final CameraPosition googlePlex = CameraPosition(
+  target: LatLng(37.42796133580664, -122.085749655962),
+  zoom: 14.4746,
+);
+
+String mapKey = 'AIzaSyAlQtqNSTnd-r7fg0PJpccZ2FFWLckeoUQ';
+
+DatabaseReference tripRequestRef;
+
+StreamSubscription<Position> homeTabPostionStream;
+
+StreamSubscription<Position> ridePositionStream;
+
+final assetsAudioPlayer = AssetsAudioPlayer();
+
+Position currentPosition;
+
+DatabaseReference rideRef;
+
+Driver currentDriverInfo;
---
+++ cab_driver/lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:cab_driver/dataprovider.dart';
+import 'package:cab_driver/globalvariabels.dart';
+import 'package:cab_driver/screens/login.dart';
+import 'package:cab_driver/screens/mainpage.dart';
+import 'package:cab_driver/screens/registration.dart';
+import 'package:cab_driver/screens/vehicleinfo.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'dart:io';
+
+import 'package:provider/provider.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final FirebaseApp app = await Firebase.initializeApp(
+    name: 'db2',
+    options: Platform.isIOS || Platform.isMacOS
+        ? FirebaseOptions(
+      appId: '1:297855924061:ios:c6de2b69b03a5be8',
+      apiKey: 'AIzaSyD_shO5mfO9lhy2TVWhfo1VUmARKlG4suk',
+      projectId: 'flutter-firebase-plugins',
+      messagingSenderId: '297855924061',
+      databaseURL: 'https://flutterfire-cd2f7.firebaseio.com',
+    )
+        : FirebaseOptions(
+      appId: '1:551565578850:android:bbcbe40dfb23366719773b',
+      apiKey: 'AIzaSyAlQtqNSTnd-r7fg0PJpccZ2FFWLckeoUQ',
+      messagingSenderId: '297855924061',
+      projectId: 'flutter-firebase-plugins',
+      databaseURL: 'https://geetaxi-a5338.firebaseio.com',
+    ),
+  );
+
+  currentFirebaseUser = FirebaseAuth.instance.currentUser;
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    
+    return ChangeNotifierProvider(
+      create: (context) => AppData(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+
+          fontFamily: 'Brand-Regular',
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        initialRoute: (currentFirebaseUser == null) ? LoginPage.id : MainPage.id,
+        routes: {
+          MainPage.id: (context) => MainPage(),
+          RegistrationPage.id: (context) => RegistrationPage(),
+          VehicleInfoPage.id: (context) => VehicleInfoPage(),
+          LoginPage.id: (context) => LoginPage(),
+        },
+      ),
+    );
+  }
+}
+
